a41b410a2835327e921f9ef81213f2cc4b66419e
maxxmudd <maxxamudd@gmail.com>
2019-12-02 03:14:38 -0700

    Significant updates
    -Skratch curve now uses a switch case instead of if/else statements.
    -Switched core variables back to int. This was necessary to utilize switch case as well as values larger than 255 for volume.
    -Calculated and graphed multiple curve modes, now 6 modes all together. Curve charts are included in the Excel file and also as an image file in the main folder.
    -Created integer arrays to hold the more complex curve data, each containig 128 elements. This simplifies the process of assigning volume to current position on both channels, and avoids on-the-fly calculations that would slow down the whole process. Trying to keep this streamlined!
    -I have identified and ordered the spare connector parts for mosts Vestax VCA mixers. I have also started working on a 3D-printable enclosure for the step down converters and Arduino Beetle.
    
    More progress soon!

--- arduino_cfx/arduino_cfx.ino
+++ arduino_cfx/arduino_cfx.ino
@@ -23,28 +23,13 @@
    Red = GROUND ||  BLACK = RX || BLACK: POWER (3.3V)
 
    A serial connection must be opened at 31250 baud rate (MIDI) to read incoming
-   data from the X1 (for debugging). The Arduino IDE serial monitor cannot 
+   data from the X1 (for debugging). The Arduino IDE serial monitor cannot
    interpret information at this rate, so use PuTTY's serial monitor instead.
 
    Included in the file folder is an Excel spreadsheet with graphs that display
    the available curve modes and their values. Can be used to calculate other
    curve modes in the future.
-*/
 
-/* Update log:
- *  12/01/19: -Adjusted max volumes to match VCA output values (0 through 1023)
- *             and adjusted all calculations to fit within this range.
- *            -Worked on values for other curve modes in Excel (not implemented).
-    11/29/19: -Commented out the variable assignments that are always
-              -constant. Uncomment these for debugging in PuTTY.
-              -Added detailed description, instructions, and method of operation.
-              -Created a step to blink the onboard LED 3 times once powered on.
-    11/28/19: -Switched variable types to byte, removed cut variables.
-              -Created two blocks of code: one for sharp cut and one for smooth
-               curve. Only one of them will be executed continuously depending
-               on the state of a button press during power on.
-              -Created a function for the debugging prints.
-              -Created Excel spreadsheet for displaying curve information.
   ------------------------------------------------------------------------------
     To do:
               -Add more crossfader curve options, incorporate switch
@@ -58,9 +43,9 @@
 */
 
 // variables for fader position and volume
-byte cfx;
-byte volA;
-byte volB;
+int cfx;
+int volA;
+int volB;
 
 // output pins
 byte outA = 9;
@@ -68,6 +53,39 @@ byte outB = 11;
 
 // button state for selecting crossfader curve
 byte butState = 0;
+
+int standard[128] = { 0, 20, 40, 60, 80, 100, 120, 140, 160, 179, 199, 218, 238, 257, 276, 295, 313,
+                      332, 350, 368, 386, 404, 421, 438, 455, 472, 488, 504, 520, 536, 551, 566, 581,
+                      595, 610, 623, 637, 650, 663, 676, 688, 700, 712, 723, 734, 745, 756, 766, 776,
+                      786, 795, 804, 813, 821, 830, 838, 845, 853, 860, 867, 873, 880, 886, 892, 898,
+                      903, 908, 913, 918, 923, 927, 931, 936, 939, 943, 947, 950, 953, 956, 959, 962,
+                      965, 967, 970, 972, 974, 976, 978, 980, 982, 983, 985, 986, 988, 989, 990, 991,
+                      993, 994, 995, 995, 996, 997, 998, 999, 999, 1000, 1000, 1001, 1002, 1002, 1002,
+                      1003, 1003, 1004, 1004, 1004, 1005, 1005, 1005, 1005, 1005, 1006, 1006, 1006,
+                      1006, 1008, 1008
+                    };
+
+int sigmoid[128] = { 0, 3, 4, 5, 6, 7, 9, 10, 13, 15, 18, 22, 27, 32, 38, 46, 55, 66, 79, 94, 111, 131, 154,
+                     181, 211, 244, 281, 321, 364, 409, 456, 504, 552, 599, 644, 687, 727, 764, 797, 827,
+                     854, 877, 897, 914, 929, 942, 953, 962, 970, 976, 981, 986, 990, 993, 995, 998, 999,
+                     1001, 1002, 1003, 1004, 1005, 1005, 1008, 1008, 1008, 1008, 1008, 1008, 1008,
+                     1008, 1008, 1008, 1008, 1008, 1008, 1008, 1008, 1008, 1008, 1008, 1008, 1008,
+                     1008, 1008, 1008, 1008, 1008, 1008, 1008, 1008, 1008, 1008, 1008, 1008, 1008,
+                     1008, 1008, 1008, 1008, 1008, 1008, 1008, 1008, 1008, 1008, 1008, 1008, 1008,
+                     1008, 1008, 1008, 1008, 1008, 1008, 1008, 1008, 1008, 1008, 1008, 1008, 1008,
+                     1008, 1008, 1008, 1008, 1008, 1008
+                   };
+
+int gaussian[128] = { 0, 2, 6, 14, 25, 39, 56, 75, 97, 122, 148, 176, 205, 236, 268, 301, 335, 369, 402,
+                      436, 470, 503, 535, 567, 597, 627, 655, 683, 709, 733, 757, 779, 799, 818, 836,
+                      853, 868, 883, 896, 907, 918, 928, 937, 945, 953, 959, 965, 971, 975, 979, 983,
+                      986, 989, 992, 994, 996, 998, 999, 1000, 1002, 1003, 1003, 1004, 1008, 1008,
+                      1008, 1008, 1008, 1008, 1008, 1008, 1008, 1008, 1008, 1008, 1008, 1008, 1008,
+                      1008, 1008, 1008, 1008, 1008, 1008, 1008, 1008, 1008, 1008, 1008, 1008, 1008,
+                      1008, 1008, 1008, 1008, 1008, 1008, 1008, 1008, 1008, 1008, 1008, 1008, 1008,
+                      1008, 1008, 1008, 1008, 1008, 1008, 1008, 1008, 1008, 1008, 1008, 1008, 1008,
+                      1008, 1008, 1008, 1008, 1008, 1008, 1008, 1008, 1008, 1008, 1008
+                    };
 
 // The Behringer X1 uses a baud rate of 31250. */
 void setup() {
@@ -84,6 +102,8 @@ void setup() {
 
 void loop() {
   butState = digitalRead(10);               // check state of button at start
+  // **This will be changed to a dial for
+  // additional curve modes**
 
   if (butState == 1) {                      // transition curve mode
     while (true) {
@@ -92,19 +112,19 @@ void loop() {
 
         if (cfx == 0) {
           analogWrite(9, 0);                // set volume A to off
-          analogWrite(11, 1008);             // set volume B to max
+          analogWrite(11, 1008);            // set volume B to max
         } else if (cfx == 127) {
-          analogWrite(9, 1008);              // set volume A to max
+          analogWrite(9, 1008);             // set volume A to max
           analogWrite(11, 0);               // set volume B to off
         } else if (cfx < 63 && cfx > 0) {
-          // volB = 1008;                    // set volume A based on position
+          // volB = 1008;                   // set volume A based on position
           volA = cfx * 8;                   // volume B is set to max
           analogWrite(9, volA);
           analogWrite(11, volB);
         } else if (cfx > 64 && cfx < 127) {
           // volA = 1008;
-          volB = 1024 - (cfx * 8);           // set volume B based on position,
-          volB = 1008 + volB;                // volume A is set to max
+          volB = 1024 - (cfx * 8);          // set volume B based on position,
+          volB = 1008 + volB;               // volume A is set to max
           analogWrite(9, 1008);
           analogWrite(11, volB);
         } else {
@@ -113,36 +133,40 @@ void loop() {
           analogWrite(9, 1008);              // set both volumes to max
           analogWrite(11, 1008);
         }
-        // debug();
+        // debug();                          // print values in PuTTY
       }
     }
-  } else {                        // sharp cut mode (default)
+  } else {                           // skratch mode (default)
     while (true) {
       if (Serial.available()) {
-        cfx = Serial.read();      // get fader position
-        if (cfx == 0) {
-          // volA = 0;
-          // volB = 1008;
-          analogWrite(9, 0);      // set volume A to off
-          analogWrite(11, 1008);   // set volume B to max
+        cfx = Serial.read();         // get fader position
+        
+        switch (cfx) {               
+          case 0:
+            // volA = 0;
+            // volB = 1008;
+            analogWrite(9, 0);       // set volume A to off
+            analogWrite(11, 1008);   // set volume B to max
+            break;
+          case 127:
+            // volA = 1008;
+            // volB = 0;
+            analogWrite(9, 1008);    // set volume A to off
+            analogWrite(11, 0);      // set volume B to max
+            break;
+          default:
+            // volA = 1008;
+            // volB = 1008;
+            analogWrite(9, 1008);    // set both volumes to max
+            analogWrite(11, 1008);
+            break;
         }
-        else if (cfx == 127) {
-          // volA = 1008;
-          // volB = 0;
-          analogWrite(9, 1008);    // set volume A to max
-          analogWrite(11, 0);     // set volume B to off
-        }
-        else {
-          // volA = 1008;
-          // volB = 1008;
-          analogWrite(9, 1008);    // set both volumes to max
-          analogWrite(11, 1008);
-        }
-        // debug();
+        // debug();                  // print values in PuTTY
       }
     }
   }
 }
+
 
 void debug() {
   //  Note: This function does not work unless variables are assigned values
